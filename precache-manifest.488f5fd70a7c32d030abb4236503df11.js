bfb35150a77dc46c842277327256e46c664bbde4
Luis Cameroon <luis.cameroon@excelian.com>
2020-07-08 20:13:39 +0100

    Updates

---
+++ precache-manifest.488f5fd70a7c32d030abb4236503df11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2191bef0baf0cdd37fffba800b594e8f",
+    "url": "/FIXatdl2React/index.html"
+  },
+  {
+    "revision": "eff31233750f79230650",
+    "url": "/FIXatdl2React/static/css/2.9edc93b8.chunk.css"
+  },
+  {
+    "revision": "eff31233750f79230650",
+    "url": "/FIXatdl2React/static/js/2.cfe253fe.chunk.js"
+  },
+  {
+    "revision": "766803b33b2205f0f356a9e513c7211d",
+    "url": "/FIXatdl2React/static/js/2.cfe253fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23772287bd6bcddc6fc4",
+    "url": "/FIXatdl2React/static/js/main.14d368fa.chunk.js"
+  },
+  {
+    "revision": "64f4f858525778c980ec",
+    "url": "/FIXatdl2React/static/js/runtime-main.15c16c93.js"
+  }
+]);
